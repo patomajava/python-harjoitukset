78d0b2f446f65258162b5526f6008a9b88113895
Lauri Paasonen <lauri.os.paasonen@gmail.com>
2026-08-28 14:06:52 +0300

    Peliprojekti if else lisäys

--- peliprojekti/pelaajan_tiedot.py
+++ peliprojekti/pelaajan_tiedot.py
@@ -2,3 +2,8 @@ pelaajan_nimi = str(input("Kirjoita nimesi: "))
 pelaajan_ika = int(input("Kirjoita ikäsi: "))
 
 print(f"\nHei {pelaajan_nimi}, olet {pelaajan_ika} vuotta vanha.")
+
+# if pelaajan_ika >= 12:
+#     print(f"\nHei {pelaajan_nimi}, olet {pelaajan_ika} vuotta vanha.\nPelin ikäraja on K12, joten pystyt pelaamaan.")
+# else:
+#     print(f"\nHei {pelaajan_nimi}, olet {pelaajan_ika} vuotta vanha.\nPelin ikäraja on K12, joten et pysty pelaamaan.")
