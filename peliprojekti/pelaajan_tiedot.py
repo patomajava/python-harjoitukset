b5977c5ffe571d614646c451efc779a28c343a3f
Lauri Paasonen <lauri.os.paasonen@gmail.com>
2026-08-28 14:09:22 +0300

    Peliprojekti aloitus

--- peliprojekti/pelaajan_tiedot.py
+++ peliprojekti/pelaajan_tiedot.py
@@ -3,6 +3,7 @@ pelaajan_ika = int(input("Kirjoita ikäsi: "))
 
 print(f"\nHei {pelaajan_nimi}, olet {pelaajan_ika} vuotta vanha.")
 
+
 # if pelaajan_ika >= 12:
 #     print(f"\nHei {pelaajan_nimi}, olet {pelaajan_ika} vuotta vanha.\nPelin ikäraja on K12, joten pystyt pelaamaan.")
 # else:
