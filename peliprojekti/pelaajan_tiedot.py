f6f993952d8acbd3e61eb800c4947827f716396f
Lauri Paasonen <lauri.os.paasonen@gmail.com>
2026-09-03 17:52:34 +0300

    Peliprojekti 2 Tehtävä?

--- peliprojekti/pelaajan_tiedot.py
+++ peliprojekti/pelaajan_tiedot.py
@@ -1,10 +1,25 @@
-pelaajan_nimi = str(input("Kirjoita nimesi: "))
+pelaajan_nimi = str(input("\nKirjoita nimesi: "))
 pelaajan_ika = int(input("Kirjoita ikäsi: "))
 
-print(f"\nHei {pelaajan_nimi}, olet {pelaajan_ika} vuotta vanha.")
+if pelaajan_ika < 12:
+    print(f"\nHei {pelaajan_nimi}! Pelin ikäraja on K12. ")
+    exit()
 
+else:
+    print(f"\nHei {pelaajan_nimi}, tervetuloa pelaamaan!")
 
-# if pelaajan_ika >= 12:
-#     print(f"\nHei {pelaajan_nimi}, olet {pelaajan_ika} vuotta vanha.\nPelin ikäraja on K12, joten pystyt pelaamaan.")
-# else:
-#     print(f"\nHei {pelaajan_nimi}, olet {pelaajan_ika} vuotta vanha.\nPelin ikäraja on K12, joten et pysty pelaamaan.")
+while 0 == 0:
+    print("\nPäävalikko\n")
+    print("Tässä valikossa voit kutsua erilaisia ohjelmia:\n1) Lempipeli\n2) Lempiartisti\n0) Lopeta ohjelma")
+
+    haluttu_ohjelma = int(input("\nMitä ohjelmaa haluat kutsua: "))
+
+    if haluttu_ohjelma == 0:
+        print("Ohjelma sammuu.")
+        exit()
+    elif haluttu_ohjelma == 1:
+        lempipeli = str(input("\nMikä on lempipelisi? "))
+    elif haluttu_ohjelma == 2:
+        lempiartisti = str(input("\nKuka on lempiartistisi? "))
+    else:
+        print("Tuolla numerolla ei löytynyt ohjelmaa.")
