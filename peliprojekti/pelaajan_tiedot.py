ee4b3db1362e399752c6d0427fe0fa811a89c6d0
Lauri Paasonen <lauri.os.paasonen@gmail.com>
2026-09-06 16:27:08 +0300

    Peliprojektin valikko ja muuta testailua

--- peliprojekti/pelaajan_tiedot.py
+++ peliprojekti/pelaajan_tiedot.py
@@ -1,5 +1,8 @@
+print("\nTervetuloa pelaamaan peliä!")
 pelaajan_nimi = str(input("\nKirjoita nimesi: "))
 pelaajan_ika = int(input("Kirjoita ikäsi: "))
+lempipeli = ""
+lempiartisti = ""
 
 if pelaajan_ika < 12:
     print(f"\nHei {pelaajan_nimi}! Pelin ikäraja on K12. ")
@@ -8,18 +11,24 @@ if pelaajan_ika < 12:
 else:
     print(f"\nHei {pelaajan_nimi}, tervetuloa pelaamaan!")
 
-while 0 == 0:
+while True:
     print("\nPäävalikko\n")
-    print("Tässä valikossa voit kutsua erilaisia ohjelmia:\n1) Lempipeli\n2) Lempiartisti\n0) Lopeta ohjelma")
+    print("Voit kutsua erilaisia ohjelmia:\n1) Aloita peli\n2) Tulosta tietoni\n3) Lempipeli\n4) Lempiartisti\n0) Lopeta ohjelma")
 
-    haluttu_ohjelma = int(input("\nMitä ohjelmaa haluat kutsua: "))
+    haluttu_ohjelma = input("\nMitä ohjelmaa haluat kutsua? ")
 
-    if haluttu_ohjelma == 0:
+    if haluttu_ohjelma == "":
+        input("\nTuota ohjelmaa ei ole olemassa.")
+    elif int(haluttu_ohjelma) == 0:
         print("Ohjelma sammuu.")
         exit()
-    elif haluttu_ohjelma == 1:
-        lempipeli = str(input("\nMikä on lempipelisi? "))
-    elif haluttu_ohjelma == 2:
-        lempiartisti = str(input("\nKuka on lempiartistisi? "))
+    elif int(haluttu_ohjelma) == 1:
+        input("\nPeli on vielä kesken :( ")
+    elif int(haluttu_ohjelma) == 2:
+        input(f"\nTiedot sinusta:\n\nNimesi: {pelaajan_nimi}\nIkäsi: {pelaajan_ika}\nLempipelisi: {lempipeli}\nLempiartistisi: {lempiartisti}")
+    elif int(haluttu_ohjelma) == 3:
+        lempipeli = str(input("\nKirjoita lempipelisi: "))
+    elif int(haluttu_ohjelma) == 4:
+        lempiartisti = str(input("\nKirjoita lempiartistisi: "))
     else:
-        print("Tuolla numerolla ei löytynyt ohjelmaa.")
+        input("\nTuota ohjelmaa ei ole olemassa.")
