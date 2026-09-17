4ac335ad0ff52b949b82cf505bd3cbb1fd134696
Lauri Paasonen <lauri.o.paasonen@gmail.com>
2026-09-17 14:22:41 +0300

    Moduuli 9 tehtäviä

--- module9/tehtava4.py
+++ module9/tehtava4.py
@@ -36,7 +36,6 @@ for i in range(1, 11):
     auto = Auto(f"ABC-{i}", random.randint(100, 200))
     autot.append(auto)
 
-
 while True: 
     for i in autot:
         auto = i.kiihdytä(random.randint(-10, 15))
